81a61fd656c45702e953782cf1fb3ed057c1edbb
Matrox1821 <matiasgg1821@gmail.com>
2021-11-14 14:18:05 -0300

    añadí la api

--- src/components/pages/libros/LibrosPage.js
+++ src/components/pages/libros/LibrosPage.js
@@ -1,29 +1,45 @@
 import "./Libros.css";
-import React, { /* useState,  */ useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Card } from "../../ui/Card";
 /* const URL =
   "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key="; */
 export const LibrosPage = () => {
-  /* const [books, setBooks] = useState([]); */
+  const [books, setBooks] = useState([]);
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await axios.get(
         `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=90xqbHebJWFd1iuqpjGrUzs8I8wGl4Uw`
       );
-      console.log(res.data);
+      setBooks(res.data.results.books);
+      console.log(res.data.results.books);
     };
     fetchBooks();
   }, []);
+
   return (
     <div className="content-cards">
-      <Card Title="Primer libro" Description="Descripción del libro"></Card>
-      <Card Title="Segundo Libro" Description="Descripción del libro"></Card>
-      <Card Title="Tercer libro" Description="Descripción del libro"></Card>
-      <Card
-        Title="Cuarto libro"
-        Description="Descripción del último libro"
-      ></Card>
+      {books.map((book) => {
+        const {
+          age_group,
+          author,
+          book_image,
+          buy_links,
+          description,
+          price,
+          primary_isbn10,
+          publisher,
+          rank,
+          title,
+        } = book;
+        return (
+          <Card
+            title={title}
+            description={description}
+            book_image={book_image}
+          ></Card>
+        );
+      })}
     </div>
   );
 };
